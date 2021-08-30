9c1b18c09c4ceb64c084ad9560fa547b842a2855
Artem Pobedennyi <artem.pobedennyi309@gmail.com>
2021-08-30 17:28:18 +0900

    Updated for Multiple Minting

--- Contract/contracts/UmblNFT.sol
+++ Contract/contracts/UmblNFT.sol
@@ -2,117 +2,98 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
+import "./ERC721.sol";
+import "./ERC721Enumerable.sol";
+import "./Ownable.sol";
+import "./ReentrancyGuard.sol";
+import "./ERC165.sol";
+import "./Strings.sol";
+import "./Ownable.sol";
+import "./SafeMath.sol";
 
-contract UmblNFT is ERC721URIStorage, Ownable {    
-    // this contract's owner address
-    address payable tokenOwnerAddress;    
-    // this contract's token name
-    string public tokenName;
-    // this contract's token symbol
-    string public tokenSymbol;
-    // total number of token minted
-    uint256 public tokenCounter;
+contract UmblNFT is ERC721, ERC721Enumerable, Ownable, ReentrancyGuard {    
+    using SafeMath for uint8;
+    using SafeMath for uint256;
+    using Strings for string;
+
+    uint public constant MAX_MINTING_TOKENS = 100000;
+
+    enum TokenStatus { 
+        MINTED, 
+        PURCHASED, 
+        EQUIPPED,
+        STAKED 
+    }
+
     // token data structure    
     struct UmblData {
-        uint256 tokenId;
-        string tokenName;
-        string tokenURI;
-        address payable mintedBy;
-        address payable currentOwner;
-        address payable previousOwner;
+        uint256 id;
         uint256 price;
-        uint256 numberOfTransfers;
-        bool forSale;
+        TokenStatus status;
     }
-    // flag for reselling feature
-    bool private resellFlag;
-    // flag for allowing users to use token in game
-    bool private useFlag;
+
+    // Flag for sale feature enable/disable
+    bool public isEnabledSale = false;
 
     // map token id to umbl data
-    mapping(uint256 => UmblData) public allUmblData;
-    // check if token name exists
-    mapping(string => bool) public tokenNameExists;
-    // check if token URI exists
-    mapping(string => bool) public tokenURIExists;
+    mapping(uint256 => UmblData) public tokenUmblData;
+
+    string private _baseTokenURI;
+    uint public nextTokenId = 0;
 
     // initialize contract while deployment with contract's token name and symbol
-    constructor() ERC721("Umbl NFT", "UMBL") {
-        tokenName = name();
-        tokenSymbol = symbol();
-        tokenOwnerAddress = payable(msg.sender);
-        resellFlag = false;
-        useFlag = false;
-    }
+    constructor(string memory baseURI) ERC721("Umbl NFT", "UMBL") {
+        setBaseURI(baseURI);
+    }  
 
-    // switch resell flag for marketplace feature
-    function toggleResell() public onlyOwner {
-        if(resellFlag == true) {
-            resellFlag = false;
-        } else {
-            resellFlag = true;
-        }
-    }
-
-    // switch use flag for game feature
-    function toggleUse() public onlyOwner {
-        if(useFlag == false) {
-            useFlag = true;
-        } else {
-            useFlag = false;
-        }
-    }
-
-    // mint a new token
-    function mintUmbl(string memory _name, string memory _tokenURI,  uint256 _price)
-        public onlyOwner
-        returns (uint256)
+    /*
+    * Get the tokens owned by _owner
+    */
+    function tokensOfOwner(address _owner) 
+        external 
+        view 
+        returns(uint256[] memory ) 
     {
-        // check if this function caller is not an zero address account
-        require(msg.sender != address(0));
+        uint256 tokenCount = balanceOf(_owner);
+        if (tokenCount == 0) {
+            // Return an empty array
+            return new uint256[](0);
+        } else {
+            uint256[] memory result = new uint256[](tokenCount);
+            uint256 index;
+            for (index = 0; index < tokenCount; index++) {
+                result[index] = tokenOfOwnerByIndex(_owner, index);
+            }
+            return result;
+        }
+    }
 
-        // increase token counter
-        tokenCounter ++;
+    // Minting multiple tokens
+    function mint(uint256 numTokens, uint256 _price) 
+        public 
+        onlyOwner 
+        nonReentrant 
+    {
+        require(numTokens > 0 && numTokens <= MAX_MINTING_TOKENS, "Must mint from 1 to 100000 NFTs");
 
-        // check if a token exists with the above token id => incremented counter
-        require(!_exists(tokenCounter));
-        // check if the token URI already exists or not
-        require(!tokenURIExists[_tokenURI]);
-        // check if the token name already exists or not
-        require(!tokenNameExists[_name]);
+        // mint all of these tokens
+        for(uint i=0; i<numTokens; i++) {
+            // increase next token ID
+            nextTokenId++;
 
-        // mint the token
-        _mint(msg.sender, tokenCounter);
-        // set token URI
-        _setTokenURI(tokenCounter, _tokenURI);
+            // mint token
+            _safeMint(owner(), nextTokenId);
 
-        // make passed token URI as exists
-        tokenURIExists[_tokenURI] = true;
-        // make passed token name as exists
-        tokenNameExists[_name] = true;
+            // create a new token struct and pass it new values
+            UmblData memory newUmblData = UmblData(
+                nextTokenId,
+                _price,
+                TokenStatus.MINTED
+            );
 
-        // create a new token struct and pass it new values
-        UmblData memory newUmblData = UmblData(
-            tokenCounter,
-            _name,
-            _tokenURI,
-            payable(msg.sender),
-            payable(msg.sender),
-            payable(address(0)),
-            _price,
-            0,
-            true
-        );
-
-        // add the token id and it's struct to all tokens mapping
-        allUmblData[tokenCounter] = newUmblData;
-
-        // return token id
-        return tokenCounter;
+            // add the token id and it's struct to all tokens mapping
+            tokenUmblData[nextTokenId] = newUmblData;
+        }
     }
 
     // get owner of the token
@@ -133,16 +114,6 @@ contract UmblNFT is ERC721URIStorage, Ownable {
         return _tokenMetaData;
     }
 
-    // get total number of tokens minted so far
-    function getNumberOfTokenMinted()
-        public view
-        returns(uint256)
-    {
-        // uint256 _totalNumberOfTokensMinted = totalSupply();
-        uint256 _totalNumberOfTokensMinted = tokenCounter;
-        return _totalNumberOfTokensMinted;
-    }
-
     // get total number of tokens owned by an address
     function getTotalNumberOfTokensOwnedByAnAddress()
         public view
@@ -152,57 +123,27 @@ contract UmblNFT is ERC721URIStorage, Ownable {
         return _totalNumberOfTokensOwned;
     }
 
-    // check if the token already exists
-    function getTokenExists(uint256 _tokenId)
-        public view
-        returns(bool)
-    {
-        bool _tokenExists = _exists(_tokenId);
-        return _tokenExists;
-    }
-
-    // use token for game feature
-    function useToken(uint256 _tokenId) 
+    // Enable token isEquipped flag, lock it on saleso that lock it on sale
+    function setTokenStatus(uint256 _tokenId, TokenStatus _tokenStatus) 
         public payable
     {
-        // check if the use flag is true
-        require(useFlag == true);
-
-        // check if the function caller is not an zero address account
-        require(msg.sender != address(0));
-
-        // check if the token id of the token being bought exists or not
+        // require that token should exist
         require(_exists(_tokenId));
 
-        // get the token's owner
-        address tokenOwner = ownerOf(_tokenId);
-
-        // token's owner should not be an zero address account
-        require(tokenOwner != address(0));
-
         // check current call is same with the token's owner
-        require(tokenOwner == msg.sender);
+        require(ownerOf(_tokenId) == msg.sender);
 
         // get the token from all UmblData mapping and create a memory of it as defined
-        UmblData memory umblData = allUmblData[_tokenId];
+        UmblData memory umblData = tokenUmblData[_tokenId];
 
-        // transfer the token from owner to the contract owner
-        _transfer(tokenOwner, tokenOwnerAddress, _tokenId);
-
-        // update the token's previous owner
-        umblData.previousOwner = umblData.currentOwner;
-
-        // update the token's current owner
-        umblData.currentOwner = tokenOwnerAddress;
-
-        // update the token's transferred time
-        umblData.numberOfTransfers += 1;
+        // check current token status is not same with parameter
+        require(umblData.status != _tokenStatus);
 
         // update the token's forSale to false
-        umblData.forSale = true;
+        umblData.status = _tokenStatus;
 
         // set and update that token in the mapping
-        allUmblData[_tokenId] = umblData;
+        tokenUmblData[_tokenId] = umblData;
     }
 
     // buy a token by passing in the token's id
@@ -225,19 +166,20 @@ contract UmblNFT is ERC721URIStorage, Ownable {
         require(tokenOwner != msg.sender);
 
         // get the token from all UmblData mapping and create a memory of it as defined
-        UmblData memory umblData = allUmblData[_tokenId];
+        UmblData memory umblData = tokenUmblData[_tokenId];
 
         // price sent in to buy should be equal to or more than the token's price
         require(msg.value >= umblData.price);
 
         // token should be for sale
-        require(umblData.forSale);
+        require(umblData.status == TokenStatus.PURCHASED);
 
         // transfer the token from owner to the caller of the function (buyer)
         _transfer(tokenOwner, msg.sender, _tokenId);
 
         // get owner of the token
-        address payable sendTo = umblData.currentOwner;
+        address payable sendTo = payable(tokenOwner);
+        address payable tokenOwnerAddress = payable(owner());
 
         // get divided value of total token price
         uint256 _priceToOwner = msg.value / 10;
@@ -248,79 +190,97 @@ contract UmblNFT is ERC721URIStorage, Ownable {
 
         // send 90% token's worth of bnb to the owner
         sendTo.transfer(_priceToSeller);
-
-        // update the token's previous owner
-        umblData.previousOwner = umblData.currentOwner;
-
-        // update the token's current owner
-        umblData.currentOwner = payable(msg.sender);
-
-        // update the token's transferred time
-        umblData.numberOfTransfers += 1;
-
-        // update the token's forSale to false
-        umblData.forSale = false;
-
-        // set and update that token in the mapping
-        allUmblData[_tokenId] = umblData;
     }
 
     // update token's price
     function changeTokenPrice(uint256 _tokenId, uint256 _newPrice)
         public
     {
-        // require caller of the function is not an empty address account
-        require(msg.sender != address(0));
-
         // require that token should exist
         require(_exists(_tokenId));
 
-        // get the token's owner
-        address _tokenOwner = ownerOf(_tokenId);
-
         // check the token's owner should be equal to the caller of the function
-        require(_tokenOwner == msg.sender);
+        require(ownerOf(_tokenId) == msg.sender);
 
         // get the token's struct from mapping and create a memory of it
-        UmblData memory umblData = allUmblData[_tokenId];
+        UmblData memory umblData = tokenUmblData[_tokenId];
 
         // update the token's price with new price
         umblData.price = _newPrice;
 
         // set and update the token in the mapping
-        allUmblData[_tokenId] = umblData;
+        tokenUmblData[_tokenId] = umblData;
     }
 
-    // switch between set for sale and set not for sale
-    function toggleForSale(uint256 _tokenId)
-        public
+    function _beforeTokenTransfer(address from, address to, uint256 tokenId)
+        internal
+        override(ERC721, ERC721Enumerable)
     {
-        // require the resell flag is true
-        require(resellFlag == true);
+        super._beforeTokenTransfer(from, to, tokenId);
+    }
 
-        // require caller of the function is not an empty address account
-        require(msg.sender != address(0));
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC721, ERC721Enumerable)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
+    }
 
-        // require that token should exist
-        require(_exists(_tokenId));
+    function _baseURI() 
+        internal 
+        view 
+        override 
+        returns (string memory) 
+    {
+        return _baseTokenURI;
+    }
 
-        // get the token's owner
-        address _tokenOwner = ownerOf(_tokenId);
+    function getMintedCount() 
+        public 
+        view 
+        returns(uint) 
+    {
+        return nextTokenId;
+    }
 
-        // check the token's owner should be equal to the caller of the function
-        require(_tokenOwner == msg.sender);
+    function tokenURI(uint256 tokenId) 
+        public 
+        view 
+        override(ERC721) 
+        returns (string memory) 
+    {
+        string memory _tokenURI = super.tokenURI(tokenId);
+        return bytes(_tokenURI).length > 0 ? string(abi.encodePacked(_tokenURI, ".json")) : "";
+    }      
 
-        // get the token's struct from mapping and create a memory of it
-        UmblData memory umblData = allUmblData[_tokenId];
+    function _setBaseURI(string memory baseURI) 
+        internal 
+        virtual 
+    {
+        _baseTokenURI = baseURI;
+    }
 
-        // update the token's forSale
-        if(umblData.forSale) {
-            umblData.forSale = false;
-        } else {
-            umblData.forSale = true;
-        }
+    // Administrative zone
+    function setBaseURI(string memory baseURI) 
+        public 
+        onlyOwner 
+    {
+        _setBaseURI(baseURI);
+    }
 
-        // set and update the token in the mapping
-        allUmblData[_tokenId] = umblData;
+    function startMint() 
+        public 
+        onlyOwner 
+    {
+        isEnabledSale = true;
+    }
+
+    function pauseMint() 
+        public 
+        onlyOwner 
+    {
+        isEnabledSale = false;
     }
 }
